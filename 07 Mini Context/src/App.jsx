2ea319c7ca26ee6f105d78adc58c74c7fb35046d
Omkar <omkarbabar9421@gmail.com>
2025-06-21 18:53:08 +0530

    Created 07 Mini Context

---
+++ 07 Mini Context/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import UserContext from "./context/UserContext";
+import UserContextProvider from "./context/UserContextProvider";
+import Login from "./component/Login";
+import Profile from "./component/Profile";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <UserContextProvider>
+      <h1>React video for context API</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  );
+}
+
+export default App;
